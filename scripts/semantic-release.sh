8a9c712953c79c51ff1b21bf3d087f5be3e115d8
Sam McKelvie <dev@mckelvie.org>
2022-04-19 13:52:09 -0700

    minimize dependencies

--- scripts/semantic-release.sh
+++ scripts/semantic-release.sh
@@ -6,4 +6,13 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR="$( dirname "SCRIPT_DIR" )"
 
 cd "$PROJECT_DIR"
+
+git_status="$(git status --porcelain)"
+if [ -n "$git_status" ]; then
+  echo "ERROR: You have uncommitted git changes" >&2
+  exit 1
+fi
+git push
+
 GH_TOKEN="$(cat "$HOME/.private/github-semantic-versioning_token.txt")" semantic-release "$@"
+git push
